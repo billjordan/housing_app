e00539ec138dc437bac411aac3a4c35b55ae6854
Bill Jordan <bill@billjordan.info>
2015-07-26 16:24:40 -0400

    initial commit

---
+++ backend/Listing.py
@@ -0,0 +1,34 @@
+import feedparser
+import re
+
+url = 'https://norfolk.craigslist.org/search/apa?format=rss'
+pattern = re.compile("/[0-9]+.html")
+
+class Listing(object):
+    
+    def __init__(self, rss_entry):
+        self.title = rss_entry["title"]
+        self.summary = rss_entry["summary"]
+        self.published = rss_entry["published_parsed"]
+        self.updated = rss_entry["updated_parsed"]
+        self.img_location = rss_entry["enc_enclosure"]
+        result = pattern.search(rss_entry["id"])
+        self.uid = rss_entry["id"][result.start() + 1:result.end()-5]
+        self.link = rss_entry["link"]
+
+
+def main():
+    feed = feedparser.parse(url)
+    entries = feed.entries
+    listings = []
+    for entry in entries:
+        try:
+            listing = Listing(entry)
+            listings.append(listing)
+        except Exception as e:
+            print(e) 
+    print(len(listings))
+    print(listings)
+
+if __name__=="__main__":
+    main()
