b930cbc37cab43429ba6b80482ac09780e4ac078
Bill Jordan <bill@billjordan.info>
2015-07-31 12:43:15 -0400

    scaffold frontend

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name housingAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the housingAppApp
+ */
+angular.module('housingAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name housingAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the housingAppApp
+ */
+angular.module('housingAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
