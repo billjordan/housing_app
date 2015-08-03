84fc886151e92663daa8ae8d473d0d899505ccef
Bill Jordan <bill@billjordan.info>
2015-08-03 15:30:06 -0400

    add previous and next listing buttons

--- frontend/app/scripts/controllers/listing.js
+++ frontend/app/scripts/controllers/listing.js
@@ -8,13 +8,31 @@
  * Controller of the housingApp
  */
 angular.module('housingApp')
-  .controller('ListingCtrl', function ($scope, $log, $routeParams, $firebaseObject, FIREBASE_URL) {
+  .controller('ListingCtrl', function ($scope, $log, $routeParams, $firebaseArray, $firebaseObject, FIREBASE_URL) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     $log.info($routeParams.listingIndex);
-    $scope.apartment_listing = $firebaseObject(new Firebase(FIREBASE_URL + "/listings/" + $routeParams.listingIndex));
-    $log.info($scope.apartment_listing);
-  });
+    
+    //get listing for listingIndex from firebase db
+    //$scope.apartmentListing = $firebaseObject(new Firebase(FIREBASE_URL + "/listings/" + $routeParams.listingIndex));
+    var apartmentListings = $firebaseArray(new Firebase(FIREBASE_URL + "/listings/"));// + $routeParams.listingIndex));
+    apartmentListings.$loaded().then(function(){
+        $log.info(apartmentListings[1]);
+        $scope.apartmentListing = apartmentListings[Number($routeParams.listingIndex)];
+        $log.info($scope.apartment_listing);
+        setPrevAndNextListing();
+    })
+        
+    //set the index of next and previous listing for links
+    function setPrevAndNextListing() {
+        var prevListing = Number($routeParams.listingIndex) - 1;   
+        var nextListing = Number($routeParams.listingIndex) + 1;   
+        if(prevListing == 0) prevListing = apartmentListings.length - 1;
+        if(nextListing == apartmentListings.length) nextListing = 0;
+        $scope.prevListing = prevListing;
+        $scope.nextListing = nextListing;
+    }
+});
