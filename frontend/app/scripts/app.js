143e5a89907f139176d3fc588432c5051cd05c85
Bill Jordan <bill@billjordan.info>
2015-08-03 14:07:47 -0400

    add angular-img-fallback and replace broken thumnails with big img

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    "firebase"
+    "firebase",
+    'dcbImgFallback'
   ])
     .constant("FIREBASE_URL", "https://housingapp.firebaseio.com/")
   .config(function ($routeProvider) {
